be1ac6f37f5f23a9cd26385a3c71dc02dfbb3ca4
Alexsandro Raphael <122562144+leexjimmy@users.noreply.github.com>
2023-02-04 21:44:09 -0300

    Add files via upload

--- finance.py
+++ finance.py
@@ -6,26 +6,39 @@ from datetime import date
 from plotly import graph_objects as go
 import plotly.express as px
 
-st.set_page_config(page_title='Finance', page_icon = "", layout = 'wide', initial_sidebar_state = 'auto')
+st.set_page_config(page_title='Finance', page_icon = "", layout = 'wide', initial_sidebar_state = 'expanded')
 # favicon being an object of the same kind as the one you should provide st.image() with (ie. a PIL array for example) or a string (url or local file path)
 
 # Função para pegar nome da ação e sigla
 def dados_acao():
-    caminho = 'https://raw.githubusercontent.com/leexjimmy/Finance-Streamlit-Application/main/acoes.csv'
+    caminho = 'C:/Users/Alex/Desktop/Projetos/Python/streamlit/App Ações/acoes.csv'
     return pd.read_csv(caminho, delimiter=';')
 
+
+# Definindo variáveis
 df = dados_acao()
+
 acao = df['snome']
 
 DATA_INICIAL = '2018-01-01'
 DATA_FINAL = date.today().strftime('%Y-%m-%d')
 
+ 
 st.markdown("# Finance")
 
 # Sidebar
-st.sidebar.title('Finance')
-acao_e = st.sidebar.selectbox('Escolha uma Ação', acao)
+st.sidebar.markdown("<h1 style='text-align: center; color: black;'>Finance</h1>", unsafe_allow_html=True)
+acao_e = st.selectbox('Escolha uma Ação', acao)
 st.sidebar.image('image.png')
+st.sidebar.markdown("<p style='text-align: center; color: black;'>Seja bem-vindo à aplicação Finance!</p>", unsafe_allow_html=True)
+st.sidebar.markdown("<p style='text-align: center; color: black;'>Para facilitar seus acessos, aqui vão algumas informações:</p>", unsafe_allow_html=True)
+
+st.sidebar.write("* A tabela mostra a movimentação da ação selecionada nos últimos 10 dias e contém informações do preço de início, menor preço, maior preço, preço de fechamento e preço ajustado, que é feito pela própria bolsa de valores. Também existe uma coluna de Balance que mostra a diferença entre o valor inicial e o valor de fechamento para cada processo.")
+st.sidebar.write("* O gráfico de linha traz informações do comportamento da ação no mercado desde o período de 2018 até os dias atuais, atualizando diáriamente de acordo com as movimentações da bolsa.")
+st.sidebar.write("* O último gráfico de barras mostra informações sobre o o balanço entre o valor de abertura e fechamento para os últimos 30 dias de operação da bolsa de valores.")
+st.sidebar.markdown("<p style='text-align: center; color: black;'>Obrigado pela atenção! E aproveite a aplicação.</p>", unsafe_allow_html=True)
+
+
 df_acao = df[df['snome'] == acao_e]
 acao_escolhida = df_acao.iloc[0]['sigla_acao']
 acao_escolhida = acao_escolhida + '.SA'
@@ -44,14 +57,13 @@ df_valores['Balance'] = df_valores['Adj Close'].values - df_valores['Open'].valu
 st.subheader('Tabela - ' + acao_e)
 st.dataframe(df_valores.tail(10), use_container_width=True)
 
-# Gráfico 1
+# Grádico da Ação
 st.subheader("Gráfico -" + acao_e)
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=df_valores['Date'],y=df_valores['Adj Close'],
 name="Preço de Fechamento", line_color='blue'))
 st.plotly_chart(fig, use_container_width=True)
 
-# Gráfico 2
 st.subheader("Balanço nos Últimos 30 dias - " + acao_e)
 fig = px.bar(df_valores.tail(30), x='Date', y='Balance', color="Balance",color_continuous_scale=px.colors.sequential.Cividis_r)
 fig.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False)
